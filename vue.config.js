64f8ecee5136e5c1ddf1f8c175bdbb43e7019835
takamitsu-iida <takamitsu.iida@gmail.com>
2019-02-11 20:26:25 +0900

    [ci skip]

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,6 @@ module.exports = {
       sass: {
         // ~ means node_modules
         data: `@import "~bulma/bulma.sass";`
-      },
-      css: {
-        data: `@import "@/node_modules/@fortawesome/fontawesome-free/css/all.css"`
       }
     }
   }
